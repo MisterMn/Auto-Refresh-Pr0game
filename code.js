66ec1762f53d6dc3df528ef13803f7ef7129d8c0
Mar Gaeding <gaeding_marcel@web.de>
2022-03-07 17:01:49 +0100

    Create code.js

---
+++ code.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Auto-Refresh Pr0game Uebersicht
+// @namespace    https://pr0game.com/game.php?page=overview
+// @version      1.0
+// @description  try to take over the world!
+// @author       MisterMoon
+// @icon         https://cdn-icons.flaticon.com/png/512/3031/premium/3031710.png?token=exp=1646668830~hmac=57ed047a4190d8daa09cad2a5abb4782
+// @grant        none
+// @include      https://pr0game.com/game.php?page=overview
+// ==/UserScript==
+
+(function() {
+    'use strict';
+setTimeout(function(){ location.reload(); }, 20*1000);
+})();
